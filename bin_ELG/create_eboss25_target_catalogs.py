3c0ec20e9e53650b64c1a48c725c24def25ef402
JohanComparat <johan.comparat@gmail.com>
2017-10-12 23:38:37 +0200

    mask V12

---
+++ bin_ELG/create_eboss25_target_catalogs.py
@@ -0,0 +1,28 @@
+import astropy.io.fits as fits
+import os
+import sys
+from os.path import join
+
+def write_target_cat(data_file, out_file):
+	ra_name_data = 'ra'
+	dec_name_data = 'dec'
+	dat     = fits.open(data_file)[1].data
+	ra_data    = dat[ra_name_data]
+	dec_data    = dat[dec_name_data]
+	starsD = (dat['mask_Tycho20Vmag10']==False)& (dat['mask_Tycho210Vmag11']==False)& (dat['mask_bright_object_rykoff']==False) & (dat['mask_Tycho211Vmag115']==False)& (dat['mask_Tycho2115Vmag120']==False) 
+	sel_data = (starsD) & (dat['eboss25']) 	
+	new_columns = dat.columns 
+	hdu2 = fits.BinTableHDU.from_columns(new_columns)
+	hdu2.data = hdu2.data[sel_data]
+	if os.path.isfile(out_file):
+		os.system("rm "+out_file)
+
+	hdu2.writeto(out_file ) # for DR2 
+
+
+data_file  = join(os.environ['OBS_REPO'], 'SDSS', 'targets/ELG/elg_190_ngc_DR5.mask.fits')
+write_target_cat(data_file, data_file[:-5]+".eboss25.fits")
+
+data_file  = join(os.environ['OBS_REPO'], 'SDSS', 'targets/ELG/elg_190_ngc_DR5_2.mask.fits')
+write_target_cat(data_file, data_file[:-5]+".eboss25.fits")
+
--- bin_ELG/ks-test-depth-value.py
+++ bin_ELG/ks-test-depth-value.py
@@ -34,11 +34,11 @@ ra_rds    = hduR[1].data[ra_name_rds]
 dec_rds    = hduR[1].data[dec_name_rds]
 
 areaD = (ra_data>135.)&(ra_data<170.)&(dec_data>21.5)&(dec_data<33)
-starsD = (hduD[1].data['mask_Tycho20Vmag10']==False)& (hduD[1].data['mask_Tycho210Vmag11']==False)& (hduD[1].data['mask_bright_object_rykoff']==False) & (hduD[1].data['mask_Tycho211Vmag115']==False) 
+starsD = (hduD[1].data['mask_Tycho20Vmag10']==False)& (hduD[1].data['mask_Tycho210Vmag11']==False)& (hduD[1].data['mask_bright_object_rykoff']==False) & (hduD[1].data['mask_Tycho211Vmag115']==False)& (dat['mask_Tycho2115Vmag120']==False) 
 sel_data = (areaD) & (starsD) 
 
 areaR = (ra_rds>135.)&(ra_rds<170.)&(dec_rds>21.5)&(dec_rds<33)
-starsR = (hduR[1].data['mask_Tycho20Vmag10']==False) & (hduR[1].data['mask_Tycho210Vmag11']==False) & (hduR[1].data['mask_bright_object_rykoff']==False) & (hduR[1].data['mask_Tycho211Vmag115']==False)
+starsR = (hduR[1].data['mask_Tycho20Vmag10']==False) & (hduR[1].data['mask_Tycho210Vmag11']==False) & (hduR[1].data['mask_bright_object_rykoff']==False) & (hduR[1].data['mask_Tycho211Vmag115']==False)& (dat['mask_Tycho2115Vmag120']==False)
 
 sel_rds = (areaR)&(starsR)
 
@@ -95,67 +95,4 @@ print ( " KS-test ra" , Dnm_ra , "H rejected ?", Dnm_ra>Dlimit_value_005 )
 print ( " KS-test dec", Dnm_dec, "H rejected ?", Dnm_dec>Dlimit_value_005 )
 
 
-
-
-def write_ascii_clusterings_catalog(out_file_name, ratelim_min = 0.015):
-  # first the data 
-  spec_SDSS = (hduD[1].data['DR14_Z']>0)&(hduD[1].data['DR14_Z_ERR']>0)&(hduD[1].data['DR14_ZWARNING']==0)
-  spec_veron = (hduD[1].data['z_veron']>0)
-  z_data[spec_veron] = hduD[1].data['z_veron'][spec_veron]
-  z_data[spec_SDSS] = duD[1].data['DR14_Z'][spec_SDSS]
-  spectro = (z_data>0)
-  starsD = (hduD[1].data['mask_Tycho20Vmag10']==False)& (hduD[1].data['mask_Tycho210Vmag11']==False)& (hduD[1].data['mask_bright_object_rykoff']==False) # (hduD[1].data['unphot-ugriz']==False)& 
-  sel_data = (ratelim_data>ratelim_min) &(hduD[1].data['mask_dr14_N'])  &(hduD[1].data['mask_dr14_N_w']>0.9) &(spectro)&(starsD) #&(ra_data>140.)&(ra_data<300.)
-  # what about the weights ?
-  # correct from TSR ?
-  # 
-  np.savetxt(out_file_name, np.transpose([ra_data[sel_data], dec_data[sel_data], z_data[sel_data]]))
-  # now the randoms 
-  starsR = (hduR[1].data['mask_Tycho20Vmag10']==False) &(hduR[1].data['mask_Tycho210Vmag11']==False) &(hduR[1].data['mask_bright_object_rykoff']==False) # (hduR[1].data['unphot-ugriz']==False)& 
-  sel_rds = (down_samp)&(ratelim_rds>ratelim_min) &(hduR[1].data['mask_dr14_N']) &(hduR[1].data['mask_dr14_N_w']>0.9) &(starsR) #&(ra_rds>140.)&(ra_rds<300.)
-  #ra_rds[sel_rds], dec_rds[sel_rds],
-  
-
-
-
-# masks
-maskdir    = join(os.environ['OBS_REPO'],'SDSS/dr14/spiders/masks')
-dict_mask  = {}
-dict_mask['mask_bright_object_rykoff'] = join(maskdir,'bright_object_mask_rykoff_pix.ply')
-dict_mask['mask_Tycho20Vmag10']   = join(maskdir, 'tycho2mask-0Vmag10.pol')
-dict_mask['mask_Tycho210Vmag11']  = join(maskdir, 'tycho2mask-10Vmag11.pol')
-dict_mask['mask_Tycho211Vmag115'] = join(maskdir, 'tycho2mask-11Vmag115.pol')
-dict_mask['mask_dr14_N'] = join(maskdir, 'mask-QSO-N-eboss_v1.9f4.ply')
-dict_mask['mask_dr14_S'] = join(maskdir, 'mask-QSO-S-eboss_v1.9f4.ply')
-
-def createBooleanColumn(name, ra, dec, dict_mask=dict_mask):
-	#print dict_mask[name]
-	mng         = mangle.Mangle(dict_mask[name])
-	polyid      = mng.polyid(ra,dec)
-	tmp         = (polyid!=-1)
-	col = fits.Column(name=name, format="L", array=tmp)
-	return col
-
-# Create the mangle mask columns: bright star mask and bright object mask :
-for mask in dict_mask.keys():
-	print mask, ra, dec, dict_mask
-	cols += createBooleanColumn(mask, ra, dec)
-
-# writes 
-hdu2 = fits.BinTableHDU.from_columns(cols)
-hdu2.writeto(out_file)
-
-
-
-in_file  = join(os.environ['OBS_REPO'], 'SDSS/dr14/spiders/target/2RXS_AllWISE_catalog_paper_2017May26_withSpectro_with2RXS_mask.fits')
-out_file  = join(os.environ['OBS_REPO'], 'SDSS/dr14/spiders/target/2RXS_AllWISE_catalog_paper_2017May26_withSpectro_with2RXS_mask_DR14area.fits')
-mask_catalog(in_file, out_file, 'ALLW_RA', 'ALLW_DEC')
-
-in_file  = join(os.environ['OBS_REPO'], 'SDSS/dr14/spiders/randoms/randoms_dr14_NGC_with2RXS_mask.fits')
-out_file  = join(os.environ['OBS_REPO'], 'SDSS/dr14/spiders/randoms/randoms_dr14_NGC_with2RXS_mask_DR14area.fits')
-mask_catalog(in_file, out_file, 'RA', 'DEC')
-
-in_file  = join(os.environ['OBS_REPO'], 'SDSS/dr14/spiders/randoms/randoms_dr14_SGC_with2RXS_mask.fits')
-out_file  = join(os.environ['OBS_REPO'], 'SDSS/dr14/spiders/randoms/randoms_dr14_SGC_with2RXS_mask_DR14area.fits')
-mask_catalog(in_file, out_file, 'RA', 'DEC')
 
