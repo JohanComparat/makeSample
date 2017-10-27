32afda3d6a74087bda7735f7e93407b9c8dfaad5
JohanComparat <johan.comparat@gmail.com>
2017-10-27 14:03:57 +0200

    update

--- bin_SPIDERS_AGN/plot_clustering.py
+++ bin_SPIDERS_AGN/plot_clustering.py
@@ -3,28 +3,88 @@ import numpy as n
 import os
 
 repo = os.path.join(os.environ['OBS_REPO'], 'SDSS/dr14/spiders/target')
-     
 
-d_N_005 = n.loadtxt(os.path.join(repo,'clustering_agn_N_RL_0.005.2pcf' ), unpack=True)  
-d_N_0216 = n.loadtxt(os.path.join(repo,'clustering_agn_N_RL_0.0216.2pcf'), unpack=True)  
-d_S_005 = n.loadtxt(os.path.join(repo,'clustering_agn_S_RL_0.005.2pcf' ), unpack=True)  
-d_S_015 = n.loadtxt(os.path.join(repo,'clustering_agn_S_RL_0.015.2pcf' ), unpack=True)
+lrg = n.loadtxt('/data36s/comparat/SDSS/LSS/LRG/lowz_boss/galaxy.2pcf', unpack=True)     
+qso = n.loadtxt('/data36s/comparat/SDSS/LSS/BigMD_QSO/BigMDPL-QSOZ_Y1Q_1481.43deg2.2pcf', unpack=True)
+cmass = n.loadtxt('/data36s/comparat/SDSS/LSS/LRG/cmass_boss/DR12v5_CMASS_North_rd0.monopole.2pcf', unpack=True)
 
-ref = n.loadtxt(os.path.join(os.environ['OBS_REPO'], 'SDSS/dr14/spiders/clustering/elg_favole_2016.monopole'), unpack=True)
+path = "/data36s/comparat/SDSS/dr14/spiders/clustering_measurements/2rxs_all/clustering_agn_RL_0.005_LX_440.2pcf"
+d440_0 = n.loadtxt(path , unpack=True) 
+path = "/data36s/comparat/SDSS/dr14/spiders/clustering_measurements/2rxs_EXI10/clustering_agn_RL_0.005_LX_440.2pcf"
+d440_1 = n.loadtxt(path , unpack=True)
+path = "/data36s/comparat/SDSS/dr14/spiders/clustering_measurements/2rxs_EXI10_pany05/clustering_agn_RL_0.005_LX_440.2pcf"
+d440_2 = n.loadtxt(path , unpack=True)
+
+path = "/data36s/comparat/SDSS/dr14/spiders/clustering_measurements/2rxs_all/clustering_agn_RL_0.005_LX_443.2pcf"
+d443_0 = n.loadtxt(path , unpack=True)  
+path = "/data36s/comparat/SDSS/dr14/spiders/clustering_measurements/2rxs_EXI10/clustering_agn_RL_0.005_LX_443.2pcf"
+d443_1 = n.loadtxt(path , unpack=True)
+path = "/data36s/comparat/SDSS/dr14/spiders/clustering_measurements/2rxs_EXI10_pany05/clustering_agn_RL_0.005_LX_443.2pcf"
+d443_2 = n.loadtxt(path , unpack=True)
+
+path = "/data36s/comparat/SDSS/dr14/spiders/clustering_measurements/2rxs_all/clustering_agn_RL_0.005_LX_446.2pcf"
+d446_0 = n.loadtxt(path , unpack=True)  
+path = "/data36s/comparat/SDSS/dr14/spiders/clustering_measurements/2rxs_EXI10/clustering_agn_RL_0.005_LX_446.2pcf"
+d446_1 = n.loadtxt(path , unpack=True)
+path = "/data36s/comparat/SDSS/dr14/spiders/clustering_measurements/2rxs_EXI10_pany05/clustering_agn_RL_0.005_LX_446.2pcf"
+d446_2 = n.loadtxt(path , unpack=True)
+ 
+ref = n.loadtxt(os.path.join(os.environ['OBS_REPO'], 'SDSS/dr14/spiders/clustering_measurements/elg_favole_2016.monopole'), unpack=True)
+
+gf440=0.89091352990878625
+gf443=0.84990345086368468
+gf446=0.80704933209279583
+
 
 p.figure(0, (6,6))
-#p.axes()
-p.plot(d_N_005 [0], d_N_005 [1], label='N_005 ')
-p.plot(d_N_0216[0], d_N_0216[1], label='N_0216')
-p.plot(d_S_005 [0], d_S_005 [1], label='S_005 ')
-p.plot(d_S_015 [0], d_S_015 [1], label='S_015 ')
-p.plot(ref [0], ref [1], label='ELG ')
-
+p.errorbar(d440_0 [0], d440_0 [1], yerr=d440_0 [1]*(d440_0[2])**(-0.5), label='LX>44.0, z=0.22, all')
+p.errorbar(d440_1 [0], d440_1 [1], yerr=d440_1 [1]*(d440_1[2])**(-0.5), label='LX>44.0, z=0.22, exi>10')
+p.errorbar(d440_2 [0], d440_2 [1], yerr=d440_2 [1]*(d440_2[2])**(-0.5), label='LX>44.0, z=0.22, p_any>0.5')
+p.plot(lrg [0], lrg [1], label='LRG z=0.28')
+p.plot(cmass [0], cmass [1], label='CMASS z=0.54')
 p.xlabel('s Mpc/h')
 p.ylabel('xi(s)')
 p.xscale('log')
 p.yscale('log')
-p.xlim((0.1,100))
+p.ylim((0.01,100))
+p.xlim((0.8,100))
 p.legend(frameon=False, loc=0)
-p.savefig(os.path.join(repo, 'clustering.png'))
+p.savefig(os.path.join(os.environ['HOME'], 'wwwDir', 'eRoMok/clustering/data', 'clustering_LX440.png'))
 p.clf()
+
+p.figure(0, (6,6))
+p.errorbar(d443_0 [0], d443_0 [1], yerr=d443_0 [1]*(d443_0[2])**(-0.5), label='LX>44.3, z=0.31, all')
+p.errorbar(d443_1 [0], d443_1 [1], yerr=d443_1 [1]*(d443_1[2])**(-0.5), label='LX>44.3, z=0.30, exi>10')
+p.errorbar(d443_2 [0], d443_2 [1], yerr=d443_2 [1]*(d443_2[2])**(-0.5), label='LX>44.3, z=0.30, p_any>0.5')
+p.plot(lrg [0], lrg [1], label='LRG z=0.28')
+p.plot(cmass [0], cmass [1], label='CMASS z=0.54')
+p.xlabel('s Mpc/h')
+p.ylabel('xi(s)')
+p.xscale('log')
+p.yscale('log')
+p.ylim((0.01,100))
+p.xlim((0.8,100))
+p.legend(frameon=False, loc=0)
+p.savefig(os.path.join(os.environ['HOME'], 'wwwDir', 'eRoMok/clustering/data', 'clustering_LX443.png'))
+p.clf()
+
+
+p.figure(0, (6,6))
+p.errorbar(d446_0 [0], d446_0 [1], yerr=d446_0 [1]*(d446_0[2])**(-0.5), label='LX>44.6, z=0.41, all')
+p.errorbar(d446_1 [0], d446_1 [1], yerr=d446_1 [1]*(d446_1[2])**(-0.5), label='LX>44.6, z=0.40, exi>10')
+p.errorbar(d446_2 [0], d446_2 [1], yerr=d446_2 [1]*(d446_2[2])**(-0.5), label='LX>44.6, z=0.40, p_any>0.5')
+p.plot(lrg [0], lrg [1], label='LRG z=0.28')
+p.plot(cmass [0], cmass [1], label='CMASS z=0.54')
+p.xlabel('s Mpc/h')
+p.ylabel('xi(s)')
+p.xscale('log')
+p.yscale('log')
+p.ylim((0.01,100))
+p.xlim((0.8,100))
+p.legend(frameon=False, loc=0)
+p.savefig(os.path.join(os.environ['HOME'], 'wwwDir', 'eRoMok/clustering/data', 'clustering_LX446.png'))
+p.clf()
+
+
+
+
