9497d889bb853497e29c168d337edda15fc75280
JohanComparat <johan.comparat@gmail.com>
2017-12-06 15:45:02 +0100

    cluster galaxies

--- bin_SPIDERS_CLUSTERS/gal_in_clusters_magneticum_mock_10rvir.py
+++ bin_SPIDERS_CLUSTERS/gal_in_clusters_magneticum_mock_10rvir.py
@@ -99,7 +99,7 @@ dC_2_z = interp1d(cosmo.comoving_distance(n.arange(0,1.3,0.0001)), n.arange(0,1.
 ######################
 # creates a histogram of each galaxy properties, three selections
 
-def get_hist(prop, ALL, INCLUS):
+def get_hist(prop, INCLUS):
 	nnInclus, bb = n.histogram(prop[INCLUS], bins=50)
 	return bb, nnInclus
 
@@ -116,134 +116,129 @@ plot_dir = os.path.join(top_dir, 'plots')
 snap_Nr, snap_z, snap_r0, snap_r1 = n.loadtxt(os.path.join(lc_dir, "wmap.geometry.dat"), unpack=True, skiprows=3)
 
 # choose a snapshot for the clusters
-id_snap = 0
-id_s = str(int(snap_Nr[id_snap])).zfill(3)
-print('opens cluster file', os.path.join(lc_dir, "wmap."+id_s+".cluster.fits"))
-hdC = fits.open(os.path.join(lc_dir, "wmap."+id_s+".cluster.fits"))
-#cl_ihal, , cl_r, cl_z_true, cl_z_obs, cl_Mvir, cl_Rvir, cl_M500, cl_R500, cl_Mstar500, cl_Mgas500, cl_T500, cl_Lx500, cl_Ysz500, cl_M200, cl_R200 = 
-cl_l       = hdC[1].data['l']
-cl_b       = hdC[1].data['b']
-cl_z_true  = hdC[1].data['z_true']
-cl_Rvir    = hdC[1].data['Rvir']
-
-print(time.time()-t0, 'done')
-#lmax = 5.
-#bmax = 5.
-#cl_area_sel =(cl_l<lmax)&(cl_b<bmax)
-
-N_Rvir = 10.
-
-rvir_min = 1800.
-rvir_max = 2200.
-cl_rvir_sel = (cl_Rvir > rvir_min)&(cl_Rvir < rvir_max)
-
-cl_numbers = n.arange(len(cl_Rvir))[cl_rvir_sel]
-print('N clusters', len(cl_numbers))
-
-# get 
-NN_inclus_Mstar = n.zeros((len(cl_numbers), 50))
-NN_inclus_Age   = n.zeros((len(cl_numbers), 50))
-NN_inclus_Z     = n.zeros((len(cl_numbers), 50))
-NN_inclus_sfr   = n.zeros((len(cl_numbers), 50))
-NN_inclus_g     = n.zeros((len(cl_numbers), 50))
-NN_inclus_r     = n.zeros((len(cl_numbers), 50))
-NN_inclus_i     = n.zeros((len(cl_numbers), 50))
-NN_inclus_ug    = n.zeros((len(cl_numbers), 50))
-NN_inclus_gr    = n.zeros((len(cl_numbers), 50))
-NN_inclus_ri    = n.zeros((len(cl_numbers), 50))
-NN_inclus_iz    = n.zeros((len(cl_numbers), 50))
-
-# choose a cluster id : 
-for id_clus in range(len(cl_numbers)):
-	t1 = time.time()
-	# snapshot id where the clusters are
-	id_snap = 0
+for id_snap in range(len(snap_Nr)):
 	id_s = str(int(snap_Nr[id_snap])).zfill(3)
-	# cluster id
-	cl_num = cl_numbers[id_clus]
-	print(id_clus, cl_num, t1)
-	# looks for objects in a cubic box around N rvir of the cluster
-	print("cluster properties", cl_z_true[cl_num], cl_Rvir[cl_num])
-	cl_Rvir_deg = cosmo.arcsec_per_kpc_comoving(snap_z[id_snap]).value*cl_Rvir[cl_num]/3600.
-	cl_z_p_N_rvir = dC_2_z( cosmo.comoving_distance(cl_z_true[cl_num]).value + N_Rvir*cl_Rvir[cl_num]/1000.)
-	cl_z_m_N_rvir = dC_2_z( cosmo.comoving_distance(cl_z_true[cl_num]).value - N_Rvir*cl_Rvir[cl_num]/1000.)
-	# get the galaxies in the cluster and outside in the same snapshot slice
+	print('opens cluster file', os.path.join(lc_dir, "wmap."+id_s+".cluster.fits"))
+	hdC = fits.open(os.path.join(lc_dir, "wmap."+id_s+".cluster.fits"))
+	#cl_ihal, , cl_r, cl_z_true, cl_z_obs, cl_Mvir, cl_Rvir, cl_M500, cl_R500, cl_Mstar500, cl_Mgas500, cl_T500, cl_Lx500, cl_Ysz500, cl_M200, cl_R200 = 
+	cl_l       = hdC[1].data['l']
+	cl_b       = hdC[1].data['b']
+	cl_z_true  = hdC[1].data['z_true']
+	cl_Rvir    = hdC[1].data['Rvir']
+
+	print(time.time()-t0, 'done')
+
+	N_Rvir = 10.
+
+	rvir_min = 1800.
+	rvir_max = 2200.
+	cl_rvir_sel = (cl_Rvir > 0) # rvir_min)&(cl_Rvir < rvir_max)
+
+	cl_numbers = n.arange(len(cl_Rvir))[cl_rvir_sel]
+	print('N clusters', len(cl_numbers))
+
+	# opens the galaxy file
 	print('opens galaxy file in the same snapshot as the cluster', os.path.join(lc_dir, "wmap."+id_s+".galaxies.fits"))
-	#isub, l, b, rr, vmax, z_true, z_obs, Mstar, sfr, u, V,  g,  r,  i,  z,  Y,  J , H  ,K  ,L  ,M, Age ,Z = n.loadtxt(os.path.join(lc_dir, "wmap."+id_s+".galaxies.fits"), unpack=True, skiprows=1)
+	id_s = str(int(snap_Nr[id_snap])).zfill(3)	
 	hd = fits.open(os.path.join(lc_dir, "wmap."+id_s+".galaxies.fits"))
-	gal_in_N_Rvir_proj = (abs(cl_l[cl_num] - hd[1].data['l'])<cl_Rvir_deg * N_Rvir ) & (abs(cl_b[cl_num] - hd[1].data['b'])<cl_Rvir_deg * N_Rvir )
-	gal_in_N_Rvir_in_cluster = (gal_in_N_Rvir_proj) & (hd[1].data['z_true'] > cl_z_m_N_rvir) & (hd[1].data['z_true'] < cl_z_p_N_rvir)
-	bb_Mstar, NN_inclus_Mstar[id_clus] =  get_hist(n.log10(hd[1].data['Mstar']), gal_in_N_Rvir_proj, gal_in_N_Rvir_in_cluster)
-	bb_Age,   NN_inclus_Age[id_clus]   =  get_hist(hd[1].data['Age'], gal_in_N_Rvir_proj, gal_in_N_Rvir_in_cluster)
-	bb_Z,     NN_inclus_Z[id_clus]     =  get_hist(hd[1].data['Z'], gal_in_N_Rvir_proj, gal_in_N_Rvir_in_cluster)
-	bb_sfr,   NN_inclus_sfr[id_clus]   =  get_hist(n.log10(hd[1].data['sfr']+0.001), gal_in_N_Rvir_proj, gal_in_N_Rvir_in_cluster)
-	bb_g,     NN_inclus_g[id_clus]     =  get_hist(hd[1].data['g'], gal_in_N_Rvir_proj, gal_in_N_Rvir_in_cluster)
-	bb_r,     NN_inclus_r[id_clus]     =  get_hist(hd[1].data['r'], gal_in_N_Rvir_proj, gal_in_N_Rvir_in_cluster)
-	bb_i,     NN_inclus_i[id_clus]     =  get_hist(hd[1].data['i'], gal_in_N_Rvir_proj, gal_in_N_Rvir_in_cluster)
-	bb_ug,     NN_inclus_ug[id_clus]   =  get_hist(hd[1].data['u']-hd[1].data['g'], gal_in_N_Rvir_proj, gal_in_N_Rvir_in_cluster)
-	bb_gr,     NN_inclus_gr[id_clus]   =  get_hist(hd[1].data['g']-hd[1].data['r'], gal_in_N_Rvir_proj, gal_in_N_Rvir_in_cluster)
-	bb_ri,     NN_inclus_ri[id_clus]   =  get_hist(hd[1].data['r']-hd[1].data['i'], gal_in_N_Rvir_proj, gal_in_N_Rvir_in_cluster)
-	bb_iz,     NN_inclus_iz[id_clus]   =  get_hist(hd[1].data['i']-hd[1].data['z'], gal_in_N_Rvir_proj, gal_in_N_Rvir_in_cluster)
+
+	cluster_id = n.zeros_like(hd[1].data['z_true'])
+	cluster_bool = (n.zeros_like(hd[1].data['z_true'])==1)
+
+	for id_clus in range(5):#len(cl_numbers)):
+		t1 = time.time()
+		# snapshot id where the clusters are
+		# cluster id
+		cl_num = cl_numbers[id_clus]
+		print(id_clus, cl_num, t1)
+		# looks for objects in a cubic box around N rvir of the cluster
+		#print("cluster properties", cl_z_true[cl_num], cl_Rvir[cl_num])
+		cl_Rvir_deg = cosmo.arcsec_per_kpc_comoving(snap_z[id_snap]).value*cl_Rvir[cl_num]/3600.
+		# redshift distance to the cluster
+		cl_z_p_N_rvir = dC_2_z( cosmo.comoving_distance(cl_z_true[cl_num]).value + N_Rvir*cl_Rvir[cl_num]/1000.)
+		cl_z_m_N_rvir = dC_2_z( cosmo.comoving_distance(cl_z_true[cl_num]).value - N_Rvir*cl_Rvir[cl_num]/1000.)
+		# get the galaxies in the cluster and outside in the same snapshot slice
+		gal_in_N_Rvir_in_cluster = (abs(cl_l[cl_num] - hd[1].data['l'])<cl_Rvir_deg * N_Rvir ) & (abs(cl_b[cl_num] - hd[1].data['b'])<cl_Rvir_deg * N_Rvir ) & (hd[1].data['z_true'] > cl_z_m_N_rvir) & (hd[1].data['z_true'] < cl_z_p_N_rvir)
+		cluster_id[gal_in_N_Rvir_in_cluster] = n.ones_like(cluster_id[gal_in_N_Rvir_in_cluster])*id_clus
+		cluster_bool[gal_in_N_Rvir_in_cluster] = (n.zeros_like(cluster_bool[gal_in_N_Rvir_in_cluster])==0)
 
 
-DATA = [NN_inclus_Mstar,
-NN_inclus_Age  ,
-NN_inclus_Z    ,
-NN_inclus_sfr  ,
-NN_inclus_g    ,
-NN_inclus_r    ,
-NN_inclus_i    ,
-NN_inclus_ug    ,
-NN_inclus_gr    ,
-NN_inclus_ri    ,
-NN_inclus_iz    ]
+	#fb = open(os.path.join(lc_dir, "results_Nrvir_"+str(N_Rvir)+".pkl"), "wb")
+	#pickle.dump(DATA, fb)
+	#fb.close()
 
-fb = open(os.path.join(lc_dir, "results_Nrvir_"+str(N_Rvir)+".pkl"), "wb")
-pickle.dump(DATA, fb)
-fb.close()
+	##fb = open(os.path.join(lc_dir, "results.pkl"), "r")
+	##DATA2 = pickle.load(fb)
+	##fb.close()
 
-#fb = open(os.path.join(lc_dir, "results.pkl"), "r")
-#DATA2 = pickle.load(fb)
-#fb.close()
+	from astropy.table import Table, Column
+	t = Table()
 
-NN_inclus_Mstar_m = n.mean(NN_inclus_Mstar, axis=0)
-NN_inclus_Age_m   = n.mean(NN_inclus_Age  , axis=0)
-NN_inclus_Z_m     = n.mean(NN_inclus_Z    , axis=0)
-NN_inclus_sfr_m   = n.mean(NN_inclus_sfr  , axis=0)
-NN_inclus_g_m     = n.mean(NN_inclus_g    , axis=0)
-NN_inclus_r_m     = n.mean(NN_inclus_r    , axis=0)
-NN_inclus_i_m     = n.mean(NN_inclus_i    , axis=0)
-NN_inclus_ug_m     = n.mean(NN_inclus_ug    , axis=0)
-NN_inclus_gr_m     = n.mean(NN_inclus_gr    , axis=0)
-NN_inclus_ri_m     = n.mean(NN_inclus_ri    , axis=0)
-NN_inclus_iz_m     = n.mean(NN_inclus_iz    , axis=0)
+	out_file = os.path.join(lc_dir, "wmap."+id_s+".galaxies_in_clusters_10Rvir.fits")
+
+	#hd[1].data[cluster_bool].shape
+
+	#hdu_cols  = fits.ColDefs([
+
+	t.add_column( Column(name='isub'      ,         data = hd[1].data['isub'  ][cluster_bool] ) )
+	t.add_column( Column(name='l'         ,         data = hd[1].data['l'     ][cluster_bool] ) )
+	t.add_column( Column(name='b'         ,         data = hd[1].data['b'     ][cluster_bool] ) )
+	t.add_column( Column(name='rr'        ,         data = hd[1].data['rr'    ][cluster_bool] ) )
+	t.add_column( Column(name='vmax'      ,         data = hd[1].data['vmax'  ][cluster_bool] ) )
+	t.add_column( Column(name='z_true'    ,         data = hd[1].data['z_true'][cluster_bool] ) )
+	t.add_column( Column(name='z_obs'     ,         data = hd[1].data['z_obs' ][cluster_bool] ) )
+	t.add_column( Column(name='Mstar'     ,         data = hd[1].data['Mstar' ][cluster_bool] ) )
+	t.add_column( Column(name='sfr'       ,         data = hd[1].data['sfr'   ][cluster_bool] ) )
+	t.add_column( Column(name='u'         ,         data = hd[1].data['u'     ][cluster_bool] ) )
+	t.add_column( Column(name='V'         ,         data = hd[1].data['V'     ][cluster_bool] ) )
+	t.add_column( Column(name='g'         ,         data = hd[1].data['g'     ][cluster_bool] ) )
+	t.add_column( Column(name='r'         ,         data = hd[1].data['r'     ][cluster_bool] ) )
+	t.add_column( Column(name='i'         ,         data = hd[1].data['i'     ][cluster_bool] ) )
+	t.add_column( Column(name='z'         ,         data = hd[1].data['z'     ][cluster_bool] ) )
+	t.add_column( Column(name='Y'         ,         data = hd[1].data['Y'     ][cluster_bool] ) )
+	t.add_column( Column(name='J'         ,         data = hd[1].data['J'     ][cluster_bool] ) )
+	t.add_column( Column(name='H'         ,         data = hd[1].data['H'     ][cluster_bool] ) )
+	t.add_column( Column(name='K'         ,         data = hd[1].data['K'     ][cluster_bool] ) )
+	t.add_column( Column(name='L'         ,         data = hd[1].data['L'     ][cluster_bool] ) )
+	t.add_column( Column(name='M'         ,         data = hd[1].data['M'     ][cluster_bool] ) )
+	t.add_column( Column(name='Age'       ,         data = hd[1].data['Age'   ][cluster_bool] ) )
+	t.add_column( Column(name='Z'         ,         data = hd[1].data['Z'     ][cluster_bool] ) )
+	t.add_column( Column(name='cluster_id',         data = cluster_id[cluster_bool] )           )
+
+	if os.path.isfile(out_file):
+		os.system("rm "+out_file)
+
+	t.write(out_file, format='fits')
+
+
+sys.exit()
 
 area = 129600./n.pi/8
 
-def plot_hist(bb, N_in, prop):
+def plot_hist(DATA, prop):
 	x = (bb[1:]+bb[:-1])/2.
 	p.figure(2, (8,6))
 	p.axes([0.18, 0.18, 0.75, 0.75])
-	p.plot(x, N_in/area, label='members', ls='solid', lw=3)
+	p.hist(DATA, bins=50, label='members', ls='solid', lw=3)
 	p.xlabel(prop)
 	p.yscale('log')
 	p.ylabel('Number in 10 Rvir / deg2')
-	p.title(r'clusters with $1.8<rvir/Mpc<2.2$ at $z\sim0.25$')
+	p.title(r'clusters at $z\sim0.25$')
 	p.legend(frameon=False)
-	p.savefig(os.path.join(plot_dir, 'hist_'+str(N_Rvir)+'_'+prop+'.png'))
+	p.savefig(os.path.join(plot_dir, 'hist_all_clusters_'+str(N_Rvir)+'_'+prop+'.png'))
 	p.clf()
 
-plot_hist(bb_ug, NN_inclus_ug_m, "U-G ABS MAG")
-plot_hist(bb_gr, NN_inclus_gr_m, "G-R ABS MAG")
-plot_hist(bb_ri, NN_inclus_ri_m, "R-I ABS MAG")
-plot_hist(bb_iz, NN_inclus_iz_m, "I-Z ABS MAG")
-plot_hist(bb_g, NN_inclus_g_m,   "G ABS MAG")
-plot_hist(bb_r, NN_inclus_r_m,   "R ABS MAG")
-plot_hist(bb_i, NN_inclus_i_m,   "I ABS MAG")
-plot_hist(bb_Mstar, NN_inclus_Mstar_m, 'Stellar mass')
-plot_hist(bb_Age, NN_inclus_Age_m   , 'Stellar Age')
-plot_hist(bb_Z, NN_inclus_Z_m     ,'Stellar metallicity')
-plot_hist(bb_sfr, NN_inclus_sfr_m   , 'Star formation rate')
+plot_hist(hd[1].data['u'][cluster_bool]-hd[1].data['g'][cluster_bool], "U-G ABS MAG")
+plot_hist(hd[1].data['g'][cluster_bool]-hd[1].data['r'][cluster_bool], "G-R ABS MAG")
+plot_hist(hd[1].data['r'][cluster_bool]-hd[1].data['i'][cluster_bool], "R-I ABS MAG")
+plot_hist(hd[1].data['i'][cluster_bool]-hd[1].data['z'][cluster_bool], "I-Z ABS MAG")
+plot_hist(hd[1].data['g'][cluster_bool], "G ABS MAG")
+plot_hist(hd[1].data['r'][cluster_bool], "R ABS MAG")
+plot_hist(hd[1].data['i'][cluster_bool], "I ABS MAG")
+plot_hist(n.log10(hd[1].data['Mstar'][cluster_bool]), 'Stellar mass')
+plot_hist(hd[1].data['Age'][cluster_bool], 'Stellar Age')
+plot_hist(hd[1].data['Z'][cluster_bool], 'Stellar metallicity')
+plot_hist(n.log10(hd[1].data['sfr'][cluster_bool]+0.001), 'Star formation rate')
 
 
 
