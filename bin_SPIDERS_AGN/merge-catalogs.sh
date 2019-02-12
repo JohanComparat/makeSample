06e28693e97bc3e659e4c1fb9c702921c20529c9
JohanComparat <johan.comparat@gmail.com>
2019-02-12 12:15:11 +0100

    compile spec z catalog

--- bin_SPIDERS_AGN/merge-catalogs.sh
+++ bin_SPIDERS_AGN/merge-catalogs.sh
@@ -1,21 +1,44 @@
+#!/bin/sh
+
 # ds52
 # screen -r AGN_SPIDERS
-	
 # Match with latest SDSS pipeline results, find 7208 counterparts
 
+CAT_IN=/data44s/eroAGN_WG_DATA/DATA/photometry/catalogs/2RXS/2RXS_AllWISE_catalog_paper_2017May26.fits.gz
+
+CAT_SPEC_v5_11_0=/data44s/eroAGN_WG_DATA/DATA/spectroscopy/catalogs/SDSS/v5_11_0/spAll-v5_11_0.fits
+CAT_SPEC_26=/data44s/eroAGN_WG_DATA/DATA/spectroscopy/catalogs/SDSS/26/specObj-SDSS-dr14.fits
+
+CAT_TMP=/data36s/comparat/AGN_clustering/catalogs/tmp.fits 
+CAT_OUT=/data36s/comparat/AGN_clustering/catalogs/2RXS_AllWISE_catalog_paper_2017May26_v5_11_0_sdss_26.fits 
+
 stilts tmatch2 \
-in1=/data36s/comparat/SDSS/dr14/spiders/target/2RXS_AllWISE_catalog_paper_2017May26.fits.gz ifmt1=fits \
-in2=/data37s/SDSS/catalogs/spAll_short_ZW04-v5_10_10.fits ifmt2=fits \
-matcher=sky params="2" join=all1 find=best \
+in1=$CAT_IN ifmt1=fits \
+in2=$CAT_SPEC_v5_11_0 ifmt2=fits \
+matcher=sky params="1" join=all1 find=best \
 values1="ALLW_RA ALLW_DEC" values2="PLUG_RA PLUG_DEC" \
+suffix1="" suffix2="_BOSS_v5_11_0" \
 ocmd='addcol hp3 "healpixNestIndex( 3, ALLW_RA, ALLW_DEC )"' \
 ocmd='addcol hp4 "healpixNestIndex( 4, ALLW_RA, ALLW_DEC )"' \
 ocmd='addcol hp5 "healpixNestIndex( 5, ALLW_RA, ALLW_DEC )"' \
 ocmd='addcol hp6 "healpixNestIndex( 6, ALLW_RA, ALLW_DEC )"' \
 ocmd='addcol hp12 "healpixNestIndex( 12, ALLW_RA, ALLW_DEC )"' \
-ocmd='addcol in_v5_10_10 "Separation>=0"' \
+ocmd='addcol in_BOSS_v5_11_0 "Separation>=0"' \
 ocmd='delcols "Separation"' \
-out=/data36s/comparat/SDSS/dr14/spiders/target/2RXS_AllWISE_catalog_paper_2017May26_v5_10_10.fits 
+omode=out out=CAT_TMP
+
+stilts tmatch2 \
+in1=CAT_TMP ifmt1=fits \
+in2=$CAT_SPEC_26 ifmt2=fits \
+matcher=sky params="1.5" join=all1 find=best \
+values1="ALLW_RA ALLW_DEC" values2="PLUG_RA PLUG_DEC" \
+suffix1="" suffix2="_SDSS_26" \
+ocmd='addcol in_SDSS_26 "Separation>=0"' \
+ocmd='delcols "Separation"' \
+out=$CAT_OUT
+
+
+
 
 stilts tmatch2 \
 in1=/data36s/comparat/SDSS/dr14/spiders/target/2RXS_AllWISE_catalog_paper_2017May26_v5_10_10.fits ifmt1=fits \
