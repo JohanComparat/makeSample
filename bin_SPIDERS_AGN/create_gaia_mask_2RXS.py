6c3bccbe8967b6245b561008acff7ac219a19eea
JohanComparat <johan.comparat@gmail.com>
2019-02-15 10:10:32 +0100

    plot

--- bin_SPIDERS_AGN/create_gaia_mask_2RXS.py
+++ bin_SPIDERS_AGN/create_gaia_mask_2RXS.py
@@ -11,9 +11,16 @@ import astropy.cosmology as cc
 cosmo = cc.Planck13
 from astropy.coordinates import SkyCoord
 
+from sklearn.neighbors import BallTree, DistanceMetric
+from astropy.table import Table,unique
+from math import radians, cos, sin, asin, sqrt, pi
+
 in_dir  = '/data36s/comparat/AGN_clustering/catalogs/'
 out_dir = '/data36s/comparat/AGN_clustering/angular_clustering/'
 
+deg_to_rad = np.pi/180.
+arcsec = 1. / 3600.
+rs = np.arange(80)*arcsec/2.
 
 # SDSS WISE CATALOGS
 # path_2_file = '/data44s/eroAGN_WG_DATA/DATA/masks/SDSS_WISE_imageprop_nside512.fits'
@@ -60,21 +67,8 @@ for gaia_file in gaia_table_list[1:]:
 
 	x_gal_gaia = (abs(bb_gaia)>20)&(dec_gaia<80)&(dec_gaia>-80)&(bb_ecl_gaia.value>-80)
 	selection_gaia = (x_gal_gaia)
-
 	N_gaia = len(ra_gaia[selection_gaia])
 
-	# Tree to select pairs
-
-	# COUNT UNIQUE 
-
-	from sklearn.neighbors import BallTree, DistanceMetric
-	from astropy.table import Table,unique
-	from math import radians, cos, sin, asin, sqrt, pi
-
-	deg_to_rad = np.pi/180.
-
-	arcsec = 1. / 3600.
-	rs = np.arange(100)*arcsec
 
 	agn_coordinates = deg_to_rad * np.transpose([dec_data[selection_data], ra_data[selection_data]])
 	gaia_coordinates = deg_to_rad * np.transpose([dec_gaia[selection_gaia], ra_gaia[selection_gaia]])
@@ -92,4 +86,74 @@ for gaia_file in gaia_table_list[1:]:
 
 	out_data = os.path.join(out_dir , '2RXS_AllWISE_catalog_paper_2017May26_X_GAIA_'+os.path.basename(gaia_file)+'.data')
 	np.savetxt(out_data, np.transpose([rs[1:], pair_density]), header='radius_arcsec density' )
+
+
+
+# EXCEPTION for the first file, that has a broad magnitude range 1-5 that I re-cut by hand into 2 and extend the radius.
+
+gaia_file = gaia_table_list[0]
+print(gaia_file)
+hdu_G     = fits.open(gaia_file)
+ra_gaia, dec_gaia = hdu_G[1].data['ra'], hdu_G[1].data['dec']
+coords = SkyCoord(ra_gaia, dec_gaia, unit='deg', frame='icrs')
+bb_gaia = coords.galactic.b.value
+ll_gaia = coords.galactic.l.value
+bb_ecl_gaia = coords.barycentrictrueecliptic.lat
+g_mag = hdu_G[1].data['phot_g_mean_mag']
+
+x_gal_gaia = (abs(bb_gaia)>20)&(dec_gaia<80)&(dec_gaia>-80)&(bb_ecl_gaia.value>-80)
+
+
+mag_sel = (g_mag>4)
+selection_gaia = (x_gal_gaia)&(mag_sel)
+N_gaia = len(ra_gaia[selection_gaia])
+
+# Tree to select pairs
+
+# COUNT UNIQUE 
+
+agn_coordinates = deg_to_rad * np.transpose([dec_data[selection_data], ra_data[selection_data]])
+gaia_coordinates = deg_to_rad * np.transpose([dec_gaia[selection_gaia], ra_gaia[selection_gaia]])
+
+Tree_obj_Gaia = BallTree(gaia_coordinates, metric='haversine') 
+Tree_obj_AGN = BallTree(agn_coordinates , metric='haversine') 
+
+test_c = np.array([ Tree_obj_Gaia.query_radius(agn_coordinates, r = rr, count_only=True) for rr in rs ])
+
+N_pairs_total = test_c.sum(axis=1)
+Delta_N_pairs = N_pairs_total[1:]-N_pairs_total[:-1]
+area = 4.*np.pi*(rs[1:]**2 - rs[:-1]**2)
+
+pair_density = Delta_N_pairs/(area*N_data*N_gaia)
+
+out_data = os.path.join(out_dir , '2RXS_AllWISE_catalog_paper_2017May26_X_GAIA_table_4_g_5.fits.data')
+np.savetxt(out_data, np.transpose([rs[1:], pair_density]), header='radius_arcsec density' )
+
+
+
+
+mag_sel = (g_mag<=4)
+selection_gaia = (x_gal_gaia)&(mag_sel)
+N_gaia = len(ra_gaia[selection_gaia])
+
+# Tree to select pairs
+
+# COUNT UNIQUE 
+
+agn_coordinates = deg_to_rad * np.transpose([dec_data[selection_data], ra_data[selection_data]])
+gaia_coordinates = deg_to_rad * np.transpose([dec_gaia[selection_gaia], ra_gaia[selection_gaia]])
+
+Tree_obj_Gaia = BallTree(gaia_coordinates, metric='haversine') 
+Tree_obj_AGN = BallTree(agn_coordinates , metric='haversine') 
+
+test_c = np.array([ Tree_obj_Gaia.query_radius(agn_coordinates, r = rr, count_only=True) for rr in rs ])
+
+N_pairs_total = test_c.sum(axis=1)
+Delta_N_pairs = N_pairs_total[1:]-N_pairs_total[:-1]
+area = 4.*np.pi*(rs[1:]**2 - rs[:-1]**2)
+
+pair_density = Delta_N_pairs/(area*N_data*N_gaia)
+
+out_data = os.path.join(out_dir , '2RXS_AllWISE_catalog_paper_2017May26_X_GAIA_table_1_g_4.fits.data')
+np.savetxt(out_data, np.transpose([rs[1:], pair_density]), header='radius_arcsec density' )
 
---
+++ bin_SPIDERS_AGN/create_gaia_mask_2RXS_plot.py
@@ -0,0 +1,28 @@
+import os, sys, glob
+from os.path import join
+import numpy as np
+import matplotlib.pyplot as p
+
+out_dir = '/data36s/comparat/AGN_clustering/angular_clustering/'
+
+xcorr_list = np.array(glob.glob(os.path.join(out_dir , '2RXS_AllWISE_catalog_paper_2017May26_X_GAIA_table_*.fits.data')))
+xcorr_list.sort()
+
+p.figure(1, (8,5))
+
+for el in xcorr_list:
+	print(el)
+	x,y=np.loadtxt(el, unpack=True)
+	lab = os.path.basename(el)[:-10].split('_')
+	p.plot(x*3600.,y, label=lab[-3]+'<g<'+lab[-1], lw=2)
+
+p.legend(frameon=False, loc=0)
+p.xlabel('separation to star [arcseconds]')
+p.ylabel('relative density of targets')
+p.xscale('log')
+p.yscale('log')
+p.savefig(os.path.join(out_dir ,'2RXS_GAIA.png'))
+p.clf()
+
+#cp /data36s/comparat/AGN_clustering/angular_clustering/2RXS_GAIA.png /home/comparat/wwwDir/stuff/
+
