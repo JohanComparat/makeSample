19e2bcd9fb1e0f704778b0dae2cf52268dd29c44
JohanComparat <johan.comparat@gmail.com>
2019-06-13 12:35:07 +0200

    stack X agn

--- bin_SPIDERS_AGN/stack_X_AGN.py
+++ bin_SPIDERS_AGN/stack_X_AGN.py
@@ -7,19 +7,19 @@ import SpectraStackingEBOSS as sse
 
 spec_dir = join(os.environ['HOME'],"SDSS/stacks/X_AGN")
 
-file_list = n.array(glob.glob(os.path.join(spec_dir, 'full_*_zmin_00_zmax_50.asc')))
+file_list = n.array(glob.glob(os.path.join(spec_dir, 'full_*.asc')))
 
 def stack_it(specList ):
- outfile = join(spec_dir, os.path.basename(specList)[:-4]+".stack")
- print(outfile)
- test_D = n.loadtxt(specList, unpack=True)
- print(len(test_D[0]))
- if len(test_D[0])>10:
-  stack=sse.SpectraStackingEBOSS(specList, outfile, l_start=2.8, l_end=4.1 )
-  stack.createStackMatrix()
-  stack.stackSpectra()
+	outfile = join(spec_dir, os.path.basename(specList)[:-4]+".stack")
+	print(outfile)
+	test_D = n.loadtxt(specList, unpack=True)
+	print(len(test_D[0]))
+	if len(test_D[0])>10:
+		stack=sse.SpectraStackingEBOSS(specList, outfile)
+		stack.createStackMatrix()
+		stack.stackSpectra()
 
 for file_input in file_list:
- stack_it(file_input)
+	stack_it(file_input)
 
 
