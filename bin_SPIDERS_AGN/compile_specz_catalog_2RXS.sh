5a2d813a31a3d19570ffdd00862c76e32f22fa65
JohanComparat <johan.comparat@gmail.com>
2019-07-16 10:53:19 +0200

    create summary catalogs

--- bin_SPIDERS_AGN/compile_specz_catalog_2RXS.sh
+++ bin_SPIDERS_AGN/compile_specz_catalog_2RXS.sh
@@ -7,7 +7,7 @@
 CAT_IN=/data44s/eroAGN_WG_DATA/DATA/photometry/catalogs/2RXS/2RXS_AllWISE_catalog_paper_2017May26.fits.gz
 
 # spectroscopic catalogs
-CAT_SPEC_v5_11_0=/data44s/eroAGN_WG_DATA/DATA/spectroscopy/catalogs/SDSS/v5_11_0/spAll-v5_11_0.fits
+CAT_SPEC_v5_13_0=/data44s/eroAGN_WG_DATA/DATA/spectroscopy/catalogs/SDSS/v5_13_0/spAll-v5_13_0.fits
 CAT_SPEC_26=/data44s/eroAGN_WG_DATA/DATA/spectroscopy/catalogs/SDSS/26/specObj-SDSS-dr14.fits
 VV_CAT=/data44s/eroAGN_WG_DATA/DATA/spectroscopy/catalogs/veron-veron-13th-ed.fits
 CAT_SPEC_2QZ=/data44s/eroAGN_WG_DATA/DATA/spectroscopy/catalogs/2QZ/2QZ.fits
@@ -18,10 +18,10 @@ CAT_OUT=/data36s/comparat/AGN_clustering/catalogs/rxs_tmp2.fits
 CAT_SPEC_0=/data36s/comparat/AGN_clustering/catalogs/rxs_tmp3.fits 
 
 # final output name
-CAT_SPEC=/data36s/comparat/AGN_clustering/catalogs/2RXS_AllWISE_catalog_paper_2017May26_v5_11_0_sdss_26_VERON_2QZ.fits 
+CAT_SPEC=/data36s/comparat/AGN_clustering/catalogs/2RXS_AllWISE_catalog_paper_2017May26_v5_13_0_sdss_26_VERON_2QZ.fits 
 
 # masking the final catalog
-CAT_SPEC_MASKED=/data36s/comparat/AGN_clustering/catalogs/2RXS_AllWISE_catalog_paper_2017May26_v5_11_0_sdss_26_VERON_MASKED.fits 
+CAT_SPEC_MASKED=/data36s/comparat/AGN_clustering/catalogs/2RXS_AllWISE_catalog_paper_2017May26_v5_13_0_sdss_26_VERON_2QZ_MASKED.fits 
 
 #Masks
 MASK_2RXS=/data44s/eroAGN_WG_DATA/DATA/masks/2RXS_HPX_nside4096_all_ratelimit.fits
@@ -29,16 +29,16 @@ MASK_XMMSL=/data44s/eroAGN_WG_DATA/DATA/masks/XMMSL_Map_withflags.fits
 
 stilts tmatch2 \
 in1=$CAT_IN ifmt1=fits \
-in2=$CAT_SPEC_v5_11_0 ifmt2=fits \
+in2=$CAT_SPEC_v5_13_0 ifmt2=fits \
 matcher=sky params="1" join=all1 find=best \
 values1="ALLW_RA ALLW_DEC" values2="PLUG_RA PLUG_DEC" \
-suffix1="" suffix2="_BOSS_v5_11_0" \
+suffix1="" suffix2="_BOSS_v5_13_0" \
 ocmd='addcol hp3 "healpixNestIndex( 3, ALLW_RA, ALLW_DEC )"' \
 ocmd='addcol hp4 "healpixNestIndex( 4, ALLW_RA, ALLW_DEC )"' \
 ocmd='addcol hp5 "healpixNestIndex( 5, ALLW_RA, ALLW_DEC )"' \
 ocmd='addcol hp6 "healpixNestIndex( 6, ALLW_RA, ALLW_DEC )"' \
 ocmd='addcol hp12 "healpixNestIndex( 12, ALLW_RA, ALLW_DEC )"' \
-ocmd='addcol in_BOSS_v5_11_0 "Separation>=0"' \
+ocmd='addcol in_BOSS_v5_13_0 "Separation>=0"' \
 ocmd='delcols "Separation"' \
 omode=out ofmt=fits out=$CAT_TMP
 
