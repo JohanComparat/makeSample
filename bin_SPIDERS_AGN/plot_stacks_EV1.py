8a294ab947961b132e6c3eec5bf297dfa5ef3abb
JohanComparat <johan.comparat@gmail.com>
2019-11-06 20:25:00 +0100

    stacks

--- bin_SPIDERS_AGN/plot_stacks_EV1.py
+++ bin_SPIDERS_AGN/plot_stacks_EV1.py
@@ -43,6 +43,10 @@ em_lines_MS.ID = ID.astype('str')
 em_lines_MS.WL = wl
 em_lines_MS.sel = (em_lines_MS.WL>0)
 
+#em_lines = em_lines_MS
+em_lines = em_lines_VdB
+#abs_lines = abs_lines_MS
+abs_lines = abs_lines_VdB
 
 from cycler import cycler
 # 1. Setting prop cycle on default rc parameter
@@ -62,52 +66,66 @@ fig_dir = path_2_spec_dir # os.path.join(os.environ['GIT_MAKESAMPLE'], 'figures/
 #path_2_fly_dir =  os.path.join(os.environ['HOME'], 'SDSS/stacks/SPIDERS_C_GAL', 'firefly/')
 file_list = n.array( glob.glob( os.path.join( path_2_spec_dir ,'*.stack')))
 file_list.sort()
-baseNames = n.array([ os.path.basename(bn)[5:-6] for bn in file_list ])
+baseNames = n.array([ os.path.basename(bn)[:-6] for bn in file_list ])
 baseNamesSplit = n.array([ bn.split('_') for bn in baseNames ])
 uniq_type = n.unique(baseNamesSplit.T[0])
 
-def plot_stacks(file_list_i, baseNames_i, path_2_figure='x.png'):
-	print(path_2_figure)
-	p.figure(0, (12.2, 8.2), frameon=False )
+def plot_stacks(file_list_i, baseNames_i, path_2_figure='x.png', wmin=2000, wmax=9000):
+	print(path_2_figure, wmin, wmax)
+	fig=p.figure(0, (12.2, 8.2), frameon=False )
 	#p.axes([0.1, 0.1, 0.85, 0.78])
 	#p.title(baseNames_i[0].split('_')[0]) 
 	p.xlabel('wavelength [Angstrom, rest frame]') 
 	p.ylabel(r'Flux [$f_\lambda$]')
-	p.xlim=((800, 7500)) 
 	p.grid()
 	
-	for jj, (path_2_spec, baseName) in enumerate(zip(file_list_i[1::3], baseNames_i[1::3])):
+	for jj, (path_2_spec, baseName) in enumerate(zip(file_list_i, baseNames_i)):
 		d = fits.open(path_2_spec)
-		sel = (d[1].data['NspectraPerPixel'] > 0.5*n.max(d[1].data['NspectraPerPixel'])) & (d[1].data['medianStack']>0)
+		sel = (d[1].data['NspectraPerPixel'] > 0.5*n.max(d[1].data['NspectraPerPixel'])) & (d[1].data['medianStack']>0)& (d[1].data['wavelength']>wmin)&(d[1].data['wavelength']<wmax)
 		x_data = d[1].data['wavelength'][sel]
-		y_data = d[1].data['medianStack'][sel]
+		y_data_i = d[1].data['medianStack'][sel]
+		y_data = y_data_i/n.median(y_data_i)/1.3**jj
 		y_err = d[1].data['jackknifStackErrors'][sel]
 		N_spec = d[1].data['NspectraPerPixel'][sel]
-		labels = baseName.split('_')
-		p.plot(x_data, y_data/2**jj, lw=1, label=str(float(labels[2])/10.)+'<z<'+str(float(labels[4])/10.) + ', N='+str(int(n.median(N_spec))) )
+		p.plot(x_data, y_data, lw=1.5, label=baseName + ', N='+str(int(n.median(N_spec))) )
 
-	#for name, xx  in zip(em_lines.ID[em_lines.sel], em_lines.WL[em_lines.sel] ):
-		#print(xx, 20, name)
-		#p.text(x=xx, y=20, s=name, withdash=True, rotation=90, fontsize=9)#, color='red')
-		#p.plot(xx, 20, marker='|', color='k')# s=name, withdash=True, rotation=90, fontsize=9)#, color='red')
+	em_lines.sel2 = (em_lines.sel) & (em_lines.WL>wmin) & (em_lines.WL<wmax)
+	abs_lines.sel2 = (abs_lines.sel) & (abs_lines.WL>wmin) & (abs_lines.WL<wmax)
 
-	#for name, xx in zip(abs_lines.ID[abs_lines.sel], abs_lines.WL[abs_lines.sel] ):
-		#print(xx, 1, name)
-		#p.text(x=xx, y=1, s=name, withdash=True, rotation=90, fontsize=9)#, color='blue')
-		#p.plot(xx, 1, marker='|', color='k')# s=name, withdash=True, rotation=90, fontsize=9)#, color='red')
-		
+	for name, xx  in zip(em_lines.ID[em_lines.sel2], em_lines.WL[em_lines.sel2] ):
+		y_id = n.searchsorted( x_data, xx)
+		y_position = n.max(y_data[n.max([y_id-10,0]):n.min([y_id+10,len(x_data)])])
+		print(xx, name, y_id, y_position)
+		p.text(x=xx, y=1.5, s=name, withdash=True, rotation=90, fontsize=9)#, color='red')
+		p.plot(xx, 1.1, marker='|', color='k')# s=name, withdash=True, rotation=90, fontsize=9)#, color='red')
+
+	for name, xx in zip(abs_lines.ID[abs_lines.sel2], abs_lines.WL[abs_lines.sel2] ):
+		y_id = n.searchsorted( x_data, xx)
+		y_position = n.min(y_data[n.max([y_id-10,0]):n.min([y_id+10,len(x_data)])])
+		print(xx, name, y_id, y_position)
+		p.text(x=xx, y=0.08, s=name, withdash=True, rotation=90, fontsize=9)#, color='blue')
+		p.plot(xx, 0.1, marker='|', color='k')# s=name, withdash=True, rotation=90, fontsize=9)#, color='red')
+	
+	p.xlim((wmin, wmax))
 	p.yscale('log')
-	p.legend(loc=0, fontsize=8, frameon=False)
+	p.legend(loc=3, fontsize=12, frameon=True)
 	p.tight_layout()
 	p.savefig(path_2_figure)
 	p.clf()
 
+wmin = 3000
+wmax = 7000
+plot_stacks(file_list, baseNames, os.path.join(fig_dir, 'EVstacks_wrange_'+str(wmin)+'_'+str(wmax)+'.png'), wmin, wmax )
 
-for UT in uniq_type:
-	sel = ( baseNamesSplit.T[0] == UT )
-	plot_stacks(file_list[sel], baseNames[sel], path_2_figure=os.path.join(fig_dir, UT+'.png') )
+wrange = n.arange(3100, 8000, 1000)
+for wmin, wmax in zip(wrange[:-1], wrange[1:]):
+	plot_stacks(file_list, baseNames, os.path.join(fig_dir, 'EVstacks_wrange_'+str(wmin)+'_'+str(wmax)+'.png'), wmin, wmax )
 	
-#sys.exit()
+wrange = n.arange(3100, 7500, 500)
+for wmin, wmax in zip(wrange[:-1], wrange[1:]):
+	plot_stacks(file_list, baseNames, os.path.join(fig_dir, 'EVstacks_wrange_'+str(wmin)+'_'+str(wmax)+'.png'), wmin, wmax )
+	
+sys.exit()
 
 def plot_single_stack_with_lines(path_2_spec, wmin, wmax, path_2_figure, em_lines, abs_lines):
 	print(path_2_spec, path_2_figure)
@@ -128,7 +146,7 @@ def plot_single_stack_with_lines(path_2_spec, wmin, wmax, path_2_figure, em_line
 		p.axes([0.1, 0.2, 0.85, 0.78])
 		p.xlabel('wavelength [Angstrom, rest frame]')
 		p.ylabel(r'Flux [$f_\lambda$]')#, # 10^{-17}$ erg cm$^{-2}$ s$^{-1}$ A$^{-1}$]')
-		p.xlim((x_data.min()-1, x_data.max()+1)) 
+		#p.xlim((x_data.min()-1, x_data.max()+1)) 
 		p.ylim((y_data.min()-3, y_data.max()+6)) 
 		p.grid()
 
@@ -155,11 +173,7 @@ def plot_single_stack_with_lines(path_2_spec, wmin, wmax, path_2_figure, em_line
 		p.clf()
 
 for path_2_spec in file_list:
-	#em_lines = em_lines_MS
-	em_lines = em_lines_VdB
-	#abs_lines = abs_lines_MS
-	abs_lines = abs_lines_VdB
-	wrange = n.arange(100, 10000,1000)
+	wrange = n.arange(3100, 10000,1000)
 	for wmin, wmax in zip(wrange[:-1], wrange[1:]):
 		#wmin=3000
 		#wmax=4000
