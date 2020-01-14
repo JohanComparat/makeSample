75294379921e64f664b5d223a7a0a30d7d01b9d9
JohanComparat <johan.comparat@gmail.com>
2020-01-14 22:29:18 +0100

    new stacks to do

--- bin_SPIDERS_CLUSTERS/stack_clusters.py
+++ bin_SPIDERS_CLUSTERS/stack_clusters.py
@@ -5,10 +5,13 @@ import glob
 import numpy as n
 import SpectraStackingEBOSS as sse
 
-spec_dir = join(os.environ['HOME'],"SDSS/stacks/SPIDERS_C_GAL")
+spec_dir = join(os.environ['HOME'],"SDSS/stacks/SPIDERS_C_GAL_z")
 
 #file_list = n.array(glob.glob(os.path.join(spec_dir, 'spiders_C_GAL_??_z_??.ascii')))
 file_list = n.array(glob.glob(os.path.join(spec_dir, '*.csv')))
+
+file_list = n.array(glob.glob(os.path.join('/home/comparat/software/linux/makeSample', 'data/allobjects_mem075_alllx_package', '*.csv')))
+file_list.sort()
 
 def stack_it(specList ):
  outfile = join(spec_dir, os.path.basename(specList)[:-4]+".stack")
